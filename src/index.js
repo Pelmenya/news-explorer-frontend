f748983ab1db1c611a765b1ab3a852bc3d201e8c
Dmitry Lyapin <lyapindm@yandex.ru>
2019-12-30 08:56:00 +0300

    хёдер начало

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './pages/index.css';
+
+function main() {
+  /* Константы */
+
+  // инициализация страницы
+
+  // Мое второе я
+
+  // Добавление карточки
+  // Достаем пременную Promise через метод then (2. Способ) callBack
+//  console.log('I am okey');
+}
+
+main();
