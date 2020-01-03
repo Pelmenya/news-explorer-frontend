cc313ed7decd1b62ced5899e089814626f1d0c7d
Dmitry Lyapin <lyapindm@yandex.ru>
2020-01-03 12:00:00 +0300

    Карточка готова

---
+++ bem-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+
+  levels: [{ path: 'src/blocks' }],
+
+  modules: {
+    'bem-tools': {
+      plugins: {
+        create: {
+          // Настройки уровней, которые используются только
+          // в `bem-tools-create`
+          levels: {
+            blocks: {
+              // Уровень по умолчанию, создаём сущности тут,
+              // если уровень не задан
+              default: true,
+
+              // Технологии по умолчанию для создания блоков на уровне
+              techs: ['css'],
+            },
+          },
+        },
+      },
+    },
+  },
+};
