ef4eb04058a1150c29f0feca2b826f452e0b7d4b
Dmitry Lyapin <lyapindm@yandex.ru>
2020-02-05 16:21:15 +0300

    05.02.20 16.21

--- src/js/components/ArticlesIntro.js
+++ src/js/components/ArticlesIntro.js
@@ -18,12 +18,26 @@ export default class ArticlesIntro {
   _sortArticles() {
     return this._getArticles().then((articles) => {
       if (articles) {
-        console.log(articles)
-
-        const arr = articles;
         const item = {};
         item.numberArticles = articles.length;
-    
+        const obj = {};
+        Object.keys(articles).forEach((i) => {
+          obj[articles[i].keyword] = articles[i].keyword;
+        });
+        item.arrSort = [];
+        let k = 0;
+        Object.keys(obj).forEach((i) => {
+          obj[k] = 0;
+          Object.keys(articles).forEach((j) => {
+            if (obj[i] === articles[j].keyword) obj[k] += 1;
+          });
+          item.arrSort.push({
+            number: obj[k],
+            keyword: obj[i],
+          });
+          k += 1;
+        });
+        item.arrSort.sort((a, b) => b.number - a.number);
         return item;
       }
       return null;
@@ -45,28 +59,33 @@ export default class ArticlesIntro {
       element: this.container.querySelector('.articles-intro__keywords'),
       classOpened: 'articles-intro__is-opened',
     });
+
     this.articlesIntroKeyWordBegin = new Element({
-      element: this.container.querySelector('articles-intro__keyword_begin'),
+      element: this.container.querySelector('.articles-intro__keyword_begin'),
       classOpened: 'articles-intro__is-opened',
     });
+
+    console.log(this.articlesIntroKeyWordBegin);
+    this.articlesIntroKeyWordBegin.open();
     this.articlesIntroKeyWordAnd = new Element({
-      element: this.container.querySelector('articles-intro__keyword-and'),
+      element: this.container.querySelector('.articles-intro__keyword-and'),
       classOpened: 'articles-intro__is-opened',
     });
     this.articlesIntroKeyWordEnd = new Element({
-      element: this.container.querySelector('articles-intro__keyword_end'),
+      element: this.container.querySelector('.articles-intro__keyword_end'),
       classOpened: 'articles-intro__is-opened',
     });
   }
 
   render() {
     this._sortArticles().then((results) => {
+      console.log(results);
       if (results) {
         if (
           String(results.numberArticles).split('')[
             String(results.numberArticles).split('').length - 1
-          ] === '1'
-          && String(results.numberArticles).split('')[
+          ] === '1' &&
+          String(results.numberArticles).split('')[
             String(results.numberArticles).split('').length - 2
           ] !== '1'
         ) {
@@ -80,12 +99,21 @@ export default class ArticlesIntro {
               .name}, у Вас ${results.numberArticles} сохраненных статей`
           );
         }
-
-
-
-
-
-
+        let str = `${results.arrSort[0].keyword}, `;
+        if (results.arrSort[1]) str = str.concat(results.arrSort[1].keyword);
+        if (results.arrSort.length === 3) {
+          this.articlesIntroKeyWordAnd.open();
+          this.articlesIntroKeyWordEnd.setTextContent(results.arrSort[3].keyword);
+        }
+        if (results.arrSort.length > 3) {
+          this.articlesIntroKeyWordAnd.open();
+          this.articlesIntroKeyWordEnd.setTextContent(`${results.arrSort.length - 2} другим`);
+          this.articlesIntroKeyWordEnd.open();
+        }
+        this.articlesIntroKeyWordBegin.open();
+        this.articlesIntroKeyWordBegin.setTextContent(str);
+        console.log(str);
+        this.articlesIntroKeyWords.open();
       } else {
         this.articlesIntroSubTitle.setTextContent(
           `${getProfile(profileOwner).user.name}, у Вас еще нет сохраненных статей`
